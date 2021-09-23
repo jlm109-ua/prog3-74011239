537979aae2dc8e342b4981facd8ac7e8c2c17888
jlm109-ua <jlm109@alu.ua.es>
2021-09-23 10:40:55 +0200

    Anotaciones apuntadas.

--- prog3-ImperialCommander/src/model/Coordinate.java
+++ prog3-ImperialCommander/src/model/Coordinate.java
@@ -1,3 +1,7 @@
+/**
+ * @author Juan Llinares Mauri - 74011239E
+ */
+
 package model;
 
 public class Coordinate {
@@ -11,44 +15,52 @@ public class Coordinate {
 	 * */
 	private int y;
 	
-	/*
+	/**
 	 * Declaramos el constructor. Este hará uso del "this." para
 	 * referirse a sus variables "x" e "y" debido a que le 
 	 * estamos pidiéndo que cree un objeto pasándole unas 
 	 * variables con el mismo nombre. Con este uso del "this." 
 	 * cambiará el valor de sus propias variables "x" e "y" y 
 	 * creará el nuevo objeto Coordinate con esos nuevos valores.
-	 * */
+	 * @param x
+	 * @param y
+	 */
 	public Coordinate(int x,int y) {
 		this.x=x;
 		this.y=y;
 	}
 	
-	/*
+	/**
 	 * Declaramos un nuevo constructor que creará un ojecto
 	 * Coordinate a partir de otro objeto Coordinate c dado. Aquí
 	 * no usamos el "this." ya que las variables del objeto pasado
 	 * se declaran diferente que las del nuevo objeto que 
 	 * queremos crear.
-	 * */
+	 * @param x;
+	 * @param y;
+	 */
 	public Coordinate(Coordinate c) {
 		x=c.x;
 		y=c.y;
 	}
-	 /*
+	 /**
 	  * Con "getX()" y "getY()" (los getters) conseguimos saber
 	  * el valor de las variables del objeto Coordinate que 
 	  * queremos.
-	  * */
+	  * @return x
+	  */
 	public int getX() {
 		return x;
 	}
-	
+	 /**
+	  * 
+	  * @return y
+	  */
 	public int getY() {
 		return y;
 	}
 	
-	/*
+	/**
 	 * Como en Java la función para igualar dos objetos es
 	 * diferente, hacemos uso de un método del propio objeto
 	 * con el que compararemos este con el otro objeto obj
@@ -60,7 +72,7 @@ public class Coordinate {
 	 * - true si:
 	 * 	· El objeto obj es igual (no tiene porqué ser idéntico)
 	 * 	  al de nuestro método equals.
-	 * */
+	 */
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
@@ -76,14 +88,15 @@ public class Coordinate {
 		return true;
 	}
 		
-	/*
+	/**
 	 * Creamos un método String para imprimir por pantalla
 	 * los valores "x" e "y" del objeto Coordinate.
-	 * */
+	 * @return [x,y]
+	 */
 	public String toString() {
 		return "[" + x + "," + y + "]";
 	}
-	 /*
+	 /**
 	  * Creamos un método que devuelva un objeto Coordinate las
 	  * componentes del cual vienen dadas por:
 	  * - "x + c.x" para la variable x
@@ -92,14 +105,16 @@ public class Coordinate {
 	  * simplemente la suma de las coordenadas "x" e "y" que ya
 	  * tenemos más las del objeto Coordinate c que le hemos 
 	  * pasado al método.
-	  * */
+	  * @param c
+	  * @return new_c
+	  */
 	public Coordinate add(Coordinate c) {
 		Coordinate new_c(x+c.x,y+c.y);
 		
 		return new_c;
 	}
 	
-	/*
+	/**
 	 * Este método es igual que el anterior pero con la
 	 * diferencia de que le pasamos directamente el valor 
 	 * a sumar. Debemos hacer uso del "this.", al igual que 
@@ -107,7 +122,10 @@ public class Coordinate {
 	 * objeto y las pasadas se declaran de la misma forma. 
 	 * Con el "this." conseguimos que se distingan según 
 	 * de qué objeto son.
-	 * */
+	 * @param x
+	 * @param y
+	 * @return new_c
+	 */
 	public Coordinate add(int x,int y) {
 		Coordinate new_c(this.x+x,this.y+y);
 		
