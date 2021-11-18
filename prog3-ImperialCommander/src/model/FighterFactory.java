9dbc5941d91796584cd6e475eada5907de5fb88c
jlm109-ua <jlm109@alu.ua.es>
2021-11-18 12:50:43 +0100

    Actualizada.

--- prog3-ImperialCommander/src/model/FighterFactory.java
+++ prog3-ImperialCommander/src/model/FighterFactory.java
@@ -14,24 +14,12 @@ public class FighterFactory {
 	 */
 	public static Fighter createFighter(String type,Ship mother) {
 		switch(type) {
-			case "AWing":
-				Fighter A = new AWing(mother);
-				return A;
-			case "YWing":
-				Fighter Y = new YWing(mother);
-				return Y;
-			case "XWing":
-				Fighter X = new XWing(mother);
-				return X;
-			case "TIEBomber":
-				Fighter b = new TIEBomber(mother);
-				return b;
-			case "TIEFighter":
-				Fighter f = new TIEFighter(mother);
-				return f;
-			case "TIEInterceptor":
-				Fighter i = new TIEInterceptor(mother);
-				return i;
+			case "AWing" : return new AWing(mother);
+			case "YWing": return new YWing(mother);
+			case "XWing": return new XWing(mother);
+			case "TIEBomber": return new TIEBomber(mother);
+			case "TIEFighter": return new TIEFighter(mother);
+			case "TIEInterceptor": return new TIEInterceptor(mother);
 		}
 		return null;
 	}
