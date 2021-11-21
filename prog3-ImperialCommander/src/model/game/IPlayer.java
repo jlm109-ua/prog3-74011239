c5b94232db0dbfbc046f8854fff4a43986552b78
jlm109-ua <jlm109@alu.ua.es>
2021-11-21 13:50:22 +0100

    Interfaz terminada.

--- prog3-ImperialCommander/src/model/game/IPlayer.java
+++ prog3-ImperialCommander/src/model/game/IPlayer.java
@@ -1,5 +1,37 @@
 package model.game;
 
 public interface IPlayer {
-
+	/**
+	 * Asigna el tablero pasado como parametro.
+	 * @param gb Tablero.
+	 */
+	public void setBoard(GameBoard gb);
+	/**
+	 * Devuelve la nave GameShip.
+	 * @return GameShip.
+	 */
+	public GameShip getGameShip();
+	/**
+	 * Obtiene una cadena de Fighter y los inicializa.
+	 */
+	public void initFighters();
+	/**
+	 * Comprueba si la flota esta destruida.
+	 * @return true: Si la flota esta destruida. false: Si hay algun caza que no esté destruido en la flota.
+	 */
+	public boolean isFleetDestroyed();
+	/**
+	 * Devuelve la informacion de la nave.
+	 * @return String shipstring Informacion de la nave.
+	 */
+	public String showShip();
+	/**
+	 * Elimina los cazas destruidos de la nave.
+	 */
+	public void purgeFleet();
+	/**
+	 * Realiza la siguiente jugada del jugador.
+	 * @return
+	 */
+	public boolean nextPlay();
 }
