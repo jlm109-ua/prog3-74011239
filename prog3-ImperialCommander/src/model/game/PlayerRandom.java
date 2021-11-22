134fc702b0fd9bb357231159713a704705095c0c
jlm109-ua <jlm109@alu.ua.es>
2021-11-22 12:46:33 +0100

    Actualizado. Por terminar.

--- prog3-ImperialCommander/src/model/game/PlayerRandom.java
+++ prog3-ImperialCommander/src/model/game/PlayerRandom.java
@@ -1,5 +1,137 @@
 package model.game;
 
-public class PlayerRandom {
+import model.Side;
+import model.exceptions.FighterAlreadyInBoardException;
+import model.exceptions.FighterNotInBoardException;
+import model.exceptions.OutOfBoundsException;
+import model.game.exceptions.WrongFighterIdException;
 
+import java.util.List;
+
+import model.Board;
+import model.Coordinate;
+import model.RandomNumber;
+import model.Ship;
+
+public class PlayerRandom implements IPlayer {
+	private int numFighters;
+	private GameShip ship;
+	private GameBoard board;
+	
+	public PlayerRandom(Side side,int numFighters) { // ¿?¿?¿?
+		ship = new GameShip("PlayerRandom " + side + " Ship",side);
+		this.numFighters = numFighters;
+	}
+	
+	public void setBoard(GameBoard gb) {
+		board = gb;
+	}
+	
+	public GameShip getGameShip() {
+		return ship;
+	}
+	
+	public void initFighters() {
+		String fleetString = ("");
+		int n = 0;
+		
+		if(ship.getSide().equals("REBEL")) {
+			for(int i = 0;i < 3;i++) {
+				switch(i) {
+					case 0:
+						n = RandomNumber.newRandomNumber(numFighters);
+						if(n != 0)
+							fleetString += n + "/XWing:";
+					case 1:
+						n = RandomNumber.newRandomNumber(numFighters);
+						if(n != 0)
+							fleetString += n + "/YWing:";
+					case 2:
+						n = RandomNumber.newRandomNumber(numFighters);
+						if(n != 0)
+							fleetString += n + "/AWing";
+				}
+			}
+		}else {
+			for(int i = 0;i < 3;i++) {
+				switch(i) {
+					case 0:
+						n = RandomNumber.newRandomNumber(numFighters);
+						if(n != 0)
+							fleetString += n + "/TIEFighter:";
+					case 1:
+						n = RandomNumber.newRandomNumber(numFighters);
+						if(n != 0)
+							fleetString += n + "/TIEBomber:";
+					case 2:
+						n = RandomNumber.newRandomNumber(numFighters);
+						if(n != 0)
+							fleetString += n + "/TIEInterceptor";
+				}
+			}
+		}
+		if(!fleetString.equals(null)) {
+			ship.addFighters(fleetString);
+		}
+	}
+	
+	public boolean isFleetDestroyed() {
+		return ship.isFleetDestroyed();
+	}
+	
+	public String showShip() {
+		String shipstring = ("");
+			shipstring += ship.getName();
+		
+		return shipstring;
+	}
+	
+	public void purgeFleet() {
+		ship.purgeFleet();
+	}
+	
+	public boolean nextPlay() {
+		int option = RandomNumber.newRandomNumber(100);
+		int pos = 0;
+		
+		if(option == 99) {
+			//¿?
+		}else {
+			List<Integer> ids = ship.getFightersId("board");
+			pos = RandomNumber.newRandomNumber(ids.size());
+			int id = ids.get(pos);
+			
+			if(!ids.equals(null)) {
+				if(option >= 85 && option <= 98) {
+					ship.improveFighter(id,option,board); //?????
+				}else if(option >= 25 && option <= 84) {
+					Coordinate c = new Coordinate(RandomNumber.newRandomNumber(board.getSize()),RandomNumber.newRandomNumber(board.getSize()));
+					try {
+						ship.launch(id, c, board);
+					} catch (WrongFighterIdException e) {
+						e.getMessage();
+						throw new RuntimeException(e);
+					} catch (FighterAlreadyInBoardException e) {
+						e.getMessage();
+						throw new RuntimeException(e);
+					} catch (OutOfBoundsException e) {
+						e.getMessage();
+						throw new RuntimeException(e);
+					}
+				}else if(option >= 0 && option <= 24) {
+					try {
+						ship.patrol(id, board);
+					}catch(WrongFighterIdException e) {
+						e.getMessage();
+						throw new RuntimeException(e);
+					}catch(FighterNotInBoardException e) {
+						e.getMessage();
+						throw new RuntimeException(e);
+					}
+				}
+			}else {
+				System.err.println("ERROR: No Fighters found.");
+			}
+		}
+	}
 }
