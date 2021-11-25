aece18f4306d8d30c83f2d7cf405515a3aa38b99
jlm109-ua <jlm109@alu.ua.es>
2021-11-25 10:40:21 +0100

    Clase por terminar.

--- prog3-ImperialCommander/src/model/game/Game.java
+++ prog3-ImperialCommander/src/model/game/Game.java
@@ -1,5 +1,41 @@
 package model.game;
 
-public class Game {
+import java.util.Objects;
 
+import model.exceptions.InvalidSizeException;
+
+public class Game {
+	private final int BOARD_SIZE = 10;
+	private GameBoard board;
+	private IPlayer imperial;
+	private IPlayer rebel;
+	
+	public Game(IPlayer imperial,IPlayer rebel) {
+		Objects.requireNonNull(imperial);
+		Objects.requireNonNull(rebel);
+		try {
+			board = new GameBoard(BOARD_SIZE);
+			this.imperial = imperial;
+			this.rebel = rebel;
+		}catch(InvalidSizeException e) {
+			throw new RuntimeException();
+		}
+	}
+	
+	public GameBoard getGameBoard() {
+		return board;
+	}
+	
+	public Side play() {
+		boolean endGame = false;
+		imperial.initFighters();
+		rebel.initFighters();
+		
+		do {
+			// HACER IFS (if(!endGame)) PARA COMPROBAR QUE NO SE HA TERMINADO EL JUEGO Y NO PEDIR MOVIMIENTOS A LOS JUGADORES
+			// CUANDO SON INNECESARIOS
+			
+			// TERMINAR
+		}while(endGame = true);
+	}
 }
