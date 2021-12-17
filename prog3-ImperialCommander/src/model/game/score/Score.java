51f5fa12efa82a31c7a5b10fcb6de0cc971a7769
jlm109-ua <jlm109@alu.ua.es>
2021-12-17 10:31:32 +0100

    Clase añadida y terminada.

---
+++ prog3-ImperialCommander/src/model/game/score/Score.java
@@ -0,0 +1,47 @@
+package model.game.score;
+
+import model.Side;
+
+public abstract class Score<T> implements Comparable<Score<T>>{
+	int score;
+	private Side side;
+	
+	/**
+	 * Constructor de Score.
+	 * @param side
+	 */
+	public Score(Side side) {
+		this.score = 0;
+		this.side = side;
+	}
+	
+	/**
+	 * Getter de Score.
+	 * @return score
+	 */
+	public int getScore() {
+		return score;
+	}
+	
+	/**
+	 * Compara dos Score y devuelve 1 o -1 si el valor del Score pasado como parámetro es menor o mayor, respectivamente.
+	 * Si resultan ser iguales acude al compareTo de los side para detereminar si devolver 1 o -1 (IMPERIAL > REBEL). 
+	 */
+	public int compareTo(Score<T> other) {
+		if(this.score == other.score)
+			return side.compareTo(other.side);
+		else if(this.score > other.score)
+			return 1;
+		else
+			return -1;
+	}
+	
+	/**
+	 * Devuelve la cadena ""Player " + side + ": " + score".
+	 */
+	public String toString() {
+		return "Player " + side + ": " + score;
+	}
+	
+	public abstract void score(T sc);
+}
